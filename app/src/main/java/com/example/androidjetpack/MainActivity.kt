56aa887d412442ee7d0076af0f6e65939878a799
youssefBadway <y.badway@solutionplus.net>
2024-04-30 09:52:57 +0300

    add mvvm

--- app/src/main/java/com/example/androidjetpack/MainActivity.kt
+++ app/src/main/java/com/example/androidjetpack/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.WeakHashMap
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
+      //  val weatherInfo=  DatabaseResource()
     }
 }
